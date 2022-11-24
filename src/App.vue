26ebe48c6cb68a5a879fe83b5c88c0604a43a865
manuelmenama <manuel.mena090589@gmail.com>
2022-11-24 12:53:16 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+
+import { store } from "./assets/data/store"
+
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
+
+export default {
+  name: 'AppVue',
+  data() {
+    return {
+      store
+    }
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+</script>
+
+<template>
+
+  <AppHeader />
+
+  <AppMain />
+
+  <AppFooter />
+  
+</template>
+
+<style lang="scss">
+
+@use './styles/general.scss'
+
+</style>
