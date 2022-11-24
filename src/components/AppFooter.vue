e337d7d7ad35375173c273de2dfecf4cdf57a1b1
manuelmenama <manuel.mena090589@gmail.com>
2022-11-24 15:26:01 +0100

    added import bootstrap

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,6 +5,22 @@ export default {
 </script>
 
 <template>
+
+  <footer class="container">
+    <!-- componente superiore -->
+    <div class="row upper-footer">
+
+      <div class="col-2">
+        
+      </div>
+
+    </div>
+
+    <!-- componente inferiore -->
+    <div class="row lower-footer">
+
+    </div>
+  </footer>
   
 </template>
 
